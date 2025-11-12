12c41939cb0ac0739cb6c5aa4b54d3e53dfc4525
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-11-12 08:59:54 +0700

    Menambahkan styling dasar pada css lalu mengubahnya menjadi module.
    untuk menggunakan css module, kita harus import module tersebut menjadi sebuah objek bernama styles.
    
    ketika akan digunakan kita bisa memanggilkan yang dengan atribut className diikut dengan sama dengan dan kurung kurawal.
    Di dalam kurung kurawal kita bisa memanggil objek styles dengan property css yang diinginkan.
    Contoh:
    <ul className={styles.header}>
    
    CSS Module ini memberikan manfaat seperti:
    1.  Mencegah konflik penamaan, tiap penamaan ketika di-build akan memiliki kode khusus yang unik.
    contoh
    .button{color:red}
    yang berada pada file Page.module.css
    akan menjadi Page_button_xa212s
    
    2. Isolasi Komponen dan Modularitas
    Setiap react component memiliki css yang berpasangan secara khusus dengannya. jadi style dan logic component tersebut terisolasi pada satu tempat serta ketika dibutuhkan kembali bisa digunakan di tempat lain.
    
    3. Kemudahan maintenance
    dengan terkhususnya suatu module css pada suatu component, membuat modifikasi atau menghapus module tersebut tidak mempengaruhi kode secara keseluruhan.
    
    4. Ketergantungan style yang jelas
    pada css global, sulitnya kesalahan dilacak, namun ketika menggunakan module harus mengimpor module css dan merujuk styles (objek js). Jika class tidak diimpor maka style tidak tersedia.

--- fullstack-react/src/components/userProfile/userProfile.jsx
+++ fullstack-react/src/components/userProfile/userProfile.jsx
@@ -1,10 +1,10 @@
-import "./userProfile.css";
+import styles from "./userProfile.module.css";
 
 export default function UserProfile() {
   return (
     <>
       <section>
-        <ul class="user-details">
+        <ul className={`${styles["user-details"]} ${styles.border}`}>
           <li>First Name: John</li>
           <li>Last Name: Doe</li>
           <li>Skills: React, Javascript</li>
