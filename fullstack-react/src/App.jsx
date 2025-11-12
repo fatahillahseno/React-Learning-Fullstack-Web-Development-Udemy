cf47effe985fc6d5496e3299d8bb2d1ce59644f7
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-11-12 10:42:04 +0700

    Membuat UserProfile Component menggunakan data dinamis yaitu user dan user2.
    Agar data bisa digunakan secara dinamis, maka menggunakan props pada parameter UserProfile component.
    
    destructuring apa saja yang akan dimasukkan sebagai data dinamis, pada kode ialah firstName, lastName, dan skills.
    
    Mengakses data tersebut dengan kurung kurawal dan menempatkan parameter apa yang sesuai dengan kebutuhan tampilan.
    
    pada App.jsx,, data user dan user2 sudah tersedia.
    
    ketika memanggil komponen ke dalam App, panggil juga data pada user ke dalam parameter yang dituju

--- fullstack-react/src/App.jsx
+++ fullstack-react/src/App.jsx
@@ -1,10 +1,31 @@
 import UserProfile from "./components/userProfile/userProfile.jsx";
 
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  skills: "React, Node JS",
+};
+
+const user2 = {
+  firstName: "Mark",
+  lastName: "Doe",
+  skills: "Next JS, Nest JS",
+};
+
 function App() {
   return (
     <>
-      <UserProfile />
-      <h1>Hello World!</h1>
+      <UserProfile
+        firstName={user.firstName}
+        lastName={user.lastName}
+        skills={user.skills}
+      />
+
+      <UserProfile
+        firstName={user2.firstName}
+        lastName={user2.lastName}
+        skills={user2.skills}
+      />
     </>
   );
 }
--- fullstack-react/src/components/userProfile/userProfile.jsx
+++ fullstack-react/src/components/userProfile/userProfile.jsx
@@ -1,13 +1,13 @@
 import styles from "./userProfile.module.css";
 
-export default function UserProfile() {
+export default function UserProfile({ firstName, lastName, skills } = props) {
   return (
     <>
       <section>
         <ul className={`${styles["user-details"]} ${styles.border}`}>
-          <li>First Name: John</li>
-          <li>Last Name: Doe</li>
-          <li>Skills: React, Javascript</li>
+          <li>First Name: {firstName} </li>
+          <li>Last Name: {lastName} </li>
+          <li>Skills: {skills} </li>
         </ul>
       </section>
     </>
