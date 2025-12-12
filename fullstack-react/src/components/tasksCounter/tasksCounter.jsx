96aebf25e06819ea302c796f2bdb09048a55fb5b
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-12-12 10:10:08 +0700

    Membuat component TasksCounter menjadi dinamis sesuai type dan nilai countnya
    warna border dan hitungan count akan berubah sesuai type-nya.

--- fullstack-react/src/components/tasksCounter/tasksCounter.jsx
+++ fullstack-react/src/components/tasksCounter/tasksCounter.jsx
@@ -1,12 +1,22 @@
-export function TasksCounter() {
+export function TasksCounter({ type = "todo", count = 0 } = props) {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="p-6 border-solid border-red-500 border-4 rounded-full mb-4">
+      <div
+        className={`p-6 border-solid border-red-500 
+          ${type === "todo" && "border-red-500"}
+          ${type === "inProgress" && "border-orange-500"}  
+          ${type === "completed" && "border-green-500"}  
+        border-4 rounded-full mb-4`}
+      >
         <div className="min-w-10 min-h-10 text-white text-3xl text-center flex justify-center leading-10">
-          12
+          {count}
         </div>
       </div>
-      <div>ToDo</div>
+      <div>
+        {type === "todo" && <div>To Do</div>}
+        {type === "inProgress" && <div>In Progress</div>}
+        {type === "completed" && <div>Completed</div>}
+      </div>
     </div>
   );
 }
